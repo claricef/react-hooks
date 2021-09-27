f8c434e9e4443300f75bb8eb7a54ba977021e7e2
claricef <clariceoliver432@gmail.com>
2021-09-27 13:24:37 -0300

    utilizando hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+
+  const [input, setInput]  = useState(['']);
+
+  useEffect(()=>{
+    // como deixei o segundo parametro vazio ele executa assim que o componente inicia
+    // essa função busca o que está guardado no local storage e adiciona dentro do array de tarefas
+      const tarefasStorage = localStorage.getItem('tarefas');
+      if(tarefasStorage){
+        setTarefas(JSON.parse(tarefasStorage));
+      }
+  }, []);
+
+  useEffect(()=> { // sempre que tarefas sofrer alteração chama uma função
+      localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas])
+
+  const handleAdd = useCallback(() =>{ // so chama a função quando adicionar a tarefa, nao chama sempre que digita no input
+    setTarefas([...tarefas, input])
+    setInput('');
+  }, [tarefas, input]);
+
+  const totalTarefas = useMemo (() => tarefas.length, [tarefas]); // useMemo retorna valor unico 1 parametro o que quer retornar e segundo de onte quer retornar
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <ul>
+        {tarefas.map(tarefa =>
+          <li key={tarefa}>{tarefa}</li>)
+        }
+      </ul>
+      <br/>
+      <strong>Você tem {totalTarefas} tarefas</strong>
+      <br/>
+      
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd}> Adicionar</button>
+
     </div>
   );
 }
